92a3890b784ac0ac94f3fb9a8eaee4b0384b7cc6
Anthony Zadel <theazad23@gmail.com>
2015-02-17 14:14:23 -0600

    added showAlert function to app object

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 var app = {
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -14,34 +22,13 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
-
-};
-
-app.initialize();
-
-var alert = {
-message = 'Notification';
-title = 'title';
-
-    showAlert: function (message, title) {
-    if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
-    } else {
-        alert(title ? (title + ": " + message) : message);
-    }
-},
-
-initialize: function() {
     var self = this;
     this.store = new MemoryStore(function() {
         self.showAlert('Store Initialized', 'Info');
     });
     $('.search-key').on('keyup', $.proxy(this.findByName, this));
-}
+    }
 
 };
 
-alert.initialize();
+app.initialize();
