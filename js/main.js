fe60d1e72225c3cbf83aade071c27a6d89b2b770
Anthony Zadel <theazad23@gmail.com>
2015-02-17 13:56:39 -0600

    added alert
    changed location of code

--- js/main.js
+++ js/main.js
@@ -21,3 +21,24 @@ var app = {
 };
 
 app.initialize();
+
+var alert = {
+    showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+},
+
+initialize: function() {
+    var self = this;
+    this.store = new MemoryStore(function() {
+        self.showAlert('Store Initialized', 'Info');
+    });
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+}
+
+};
+
+alert.initialize();
