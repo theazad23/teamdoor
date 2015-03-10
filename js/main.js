85de3a0449f6b60bbc4dd65bc75fa201373a6f75
Anthony Zadel <theazad23@gmail.com>
2015-03-10 10:35:23 -0500

    added view routing

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var app = {
     
     registerEvents: function() {
         var self = this;
+        $(window).on('hashchange', $.proxy(this.route, this));
         // Check of browser supports touch events...
         if (document.documentElement.hasOwnProperty('ontouchstart')) {
         // ... if yes: register touch event listener to change the "selected" state of the item
@@ -22,6 +23,20 @@ var app = {
         }
     },
 
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
+    },
+
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -33,11 +48,10 @@ var app = {
 
     initialize: function() {
         var self = this;
-        //this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        //this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.detailsURL = /^#employees\/(\d{1,})/;
         this.registerEvents();
-        this.store = new LocalStorageStore(function() {
-            $('body').html(new HomeView(self.store).render().el);
+        this.store = new MemoryStore(function() {
+            self.route();
         });
     }
 
